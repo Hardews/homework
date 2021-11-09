fc6599f2ef21479d08e38b03993dd28cfe926415
Hardews <2021214014@stu.cqupt.edu.cn>
2021-11-09 08:25:06 +0800

    homework

--- Three/three_lv2/three_lv2.go
+++ Three/three_lv2/three_lv2.go
@@ -7,19 +7,27 @@ import (
 
 
 func main()  {
-	 file,err := os.Create("D:/GOProjects/src/Three/three_lv2/plan.exe")
-	 if err!= nil{
+	 file,err := os.Create("D:/GOProjects/src/Three/three_lv2/plan.exe")//创建一个新文件
+	 if err!= nil{                        //错误类型判断
 		 fmt.Println("err:",err)
 		 return
 	 }
 
-	 file.WriteString("I’m not afraid of difficulties and insist on learning programming")
-	 fileName := "D:/GOProjects/src/Three/three_lv2/plan.exe"
-	 file,err := os.OpenFile(fileName)
+	 str := "I’m not afraid of difficulties and insist on learning programming" //定义写入字符串
+	 n ,_ := file.Write([]byte(str))    //字符串转字节
+	 if err != nil{
+		 fmt.Println("err:",err)
+		 return
+	 }
+	 fmt.Println("写入字节数",n)
+	 p := make([]byte,n)            //定义一个字节切片p，使它等于n
+	 m,_:= file.ReadAt(p,0)     //从头开始读取这个切片
 	 if err!=nil {
 		fmt.Println("err:",err)
 		return
 	 }
-	 file.Read()
+	 fmt.Println("读出字节数",m)
+	 fmt.Println(string(p))        //使其转回为字符串
+	 file.Close()                  //关闭文件
 
 }
